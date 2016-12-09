bc1bc1cc5def93375a7961c2f7632b418c1c8ef1
ollie69 <oliverwilliamhall@gmail.com>
2016-12-09 15:08:23 +0000

    update 09/12/16
    oliver

--- src/customer/Customer.java
+++ src/customer/Customer.java
@@ -11,10 +11,17 @@ package customer;
  */
 public class Customer {
       static int customerID;
-    private String name;
-    private String address;
+    private String firstname;
+    private String surname;
+    private String address1;
+    private String address2;
+    private String postcode;
+    private String city;
     private String profession;
-    private Apps app;
+    private Apps app;  //remove?
+    private account number apps brought
+    private account number apps on trial     //correct so vairable are written the same accross classes
+    private String accounttype;
     private String password;
 
     
@@ -36,6 +43,8 @@ public class Customer {
     }
     
 
+      
+      
     public String getProfession() {
         return profession;
     }
@@ -44,31 +53,88 @@ public class Customer {
         this.profession = profession;
     }
 
-    public String getName() {
-        return name;
+      
+      
+      
+    public String getFirstName() {
+        return firstname;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstname) {
+        this.firstname = firstname;
     }
 
-    public String getAddress() {
-        return address;
+      
+      
+      
+    public String getSurName() {
+        return surname;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setSurName(String name) {
+        this.surname = name;
+    
+    
+    
+    
+    
+    }
+    public String getAddress1() {
+        return address1;
     }
 
-    public Apps getApp() {
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    
+    
+    
+          
+    
+    }
+     public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+  
+      
+      
+      
+      
+    public Apps getnumber() {
         return app;
     }
 
-    public void setApp(Apps anapp) {
+    public void setApp(Apps anapp) {            
         this.app = anapp;
     }
     
+      
+      
+      
+      
+    public Account getappspaid                        //number of apps paid
+        return appspaid
+    }
+
+    public void setappstrial(String appspaid)
+        this.app = appspaid;
     
+
+
+
+
+    }
+      public Account getappstrial() {                 //number of apps on trial
+        return app;
+    }
+
+    public void setApp(String appstrial)
+        this.app = appstrial;
+    }
     
     
 }
