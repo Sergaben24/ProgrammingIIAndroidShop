234bc0acb6e7aa94867acdc024c85891c8a6bc03
Salahisaq <salah.isaq@gmail.com>
2017-01-05 13:52:17 +0000

    Add files via upload

---
+++ src/app/app.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app;
+
+/**
+ *
+ * @author k1555577
+ */
+public class app {
+    public app (){
+        
+    }
+    private int AcquirePurchace;
+    private int Acquire;
+    private int numberOfPurchace;
+    private int costofPurchace;
+    private int type;
+    private int popularity;      
+    private int customer;
+    private int developerOfApp;      
+    private int appId;
+            
+
+public int getAcquirePurchace() {
+return AcquirePurchace;
+}
+public void setAcquirePurchace (int AcquirePurchace) {
+this.AcquirePurchace = AcquirePurchace;
+}
+
+public int getAcquire() {
+return Acquire;
+}
+public void setAcquire (int Acquire) {
+this.Acquire = Acquire;
+}
+
+public int getnumberOfPurchace() {
+return numberOfPurchace;
+}
+public void setnumberOfPurchace (int numberOfPurchace) {
+this.numberOfPurchace = numberOfPurchace;
+}
+public int costofPurchace() {
+return costofPurchace;
+}
+public void setcostofPurchace (int costofPurchace) {
+this.costofPurchace = costofPurchace;
+}
+public int gettype() {
+return type;
+}
+public void settype (int type) {
+this.type = type;
+}
+public int getpopularity() {
+return popularity;
+}
+public void setpopularity (int popularity) {
+this.popularity = popularity;
+}
+public int getcustomer() {
+return customer;
+}
+public void setcustomer (int customer) {
+this.customer = customer;
+}
+public int getdeveloperOfApp() {
+return developerOfApp;
+}
+public void setdeveloperOfApp (int developerOfApp) {
+this.developerOfApp = developerOfApp;
+}
+public int getappId() {
+return appId;
+}
+public void setappId(int appId) {
+this.appId = appId;
+}
+}
+
+
